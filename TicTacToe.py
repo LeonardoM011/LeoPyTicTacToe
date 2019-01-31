7c5ea54ddd1ca0dd2167f2897ab4443b8612e805
LeonardoM011 <Leonardo.leo.201@gmail.com>
2019-01-31 23:44:04 +0100

    PvsP almost done

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,6 +8,43 @@ class Game:
     def __init__(self):
         self.player = "X"
         self.opponent = "O"
+
+        self.board = []
+        for i in range(3):
+            self.board.append(["", "", ""])
+        return
+
+    def update_board(self, x, y, symbol):
+        self.board[x][y] = symbol
+        self.check_win()
+        return
+
+    def check_win(self):
+        for row in self.board:
+            string_row = row[0] + row[1] + row[2]
+            if string_row == "XXX":
+                print("X wins")
+            elif string_row == "OOO":
+                print("O wins")
+
+        for col in range(3):
+            string_col = ""
+            for row in self.board:
+                string_col += row[col]
+            if string_col == "XXX":
+                print("X wins")
+            elif string_col == "OOO":
+                print("O wins")
+
+        for i in range(2):
+            string_board = ""
+            for j in range(3):
+                string_board += self.board[j][abs((2 * i) - j)]
+            if string_board == "XXX":
+                print("X wins")
+            elif string_board == "OOO":
+                print("O wins")
+
         return
 
 
@@ -18,28 +55,26 @@ class GUI:
         self.window.resizable(width=False, height=False)
         self.game = Game()
 
-        self.button_width = None
-        self.button_height = None
-        self.button_font = None
         self.buttons = None
         self.add_buttons()
 
+        self.turn = "X"
         return
 
     def add_buttons(self):
-        self.button_width = "2"
-        self.button_height = "1"
-        self.button_font = tkinter.font.Font(family="Helvetica", size=72)
+        button_width = "2"
+        button_height = "1"
+        button_font = tkinter.font.Font(family="Helvetica", size=72)
 
         self.buttons = []
         for x in range(3):
             but = []
             for y in range(3):
                 button = tkinter.Button(master=self.window,
-                                        width=self.button_width,
-                                        height=self.button_height,
+                                        width=button_width,
+                                        height=button_height,
                                         text="",
-                                        font=self.button_font,
+                                        font=button_font,
                                         command=lambda n_x=x, n_y=y: self.button_click_event(n_x, n_y))
                 button.grid(row=x, column=y)
                 but.append(button)
@@ -48,12 +83,24 @@ class GUI:
         return
 
     def button_click_event(self, x, y):
-        self.buttons[x][y].config(text="H", state='disabled', disabledforeground='black')
+        self.buttons[x][y].config(text=self.turn, state='disabled', disabledforeground='black')
+        self.game.update_board(x, y, self.turn)
+        self.turn = self.swap_turn(self.turn)
         return
 
     def mainloop(self):
         self.window.mainloop()
         return
+
+    @staticmethod
+    def swap_turn(turn):
+        if turn == "X":
+            turn = "O"
+        elif turn == "O":
+            turn = "X"
+        else:
+            sys.exit("Error: Turn \"" + turn + "\" not valid")
+        return turn
 
 
 def main():
